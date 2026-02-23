f88384cae2792897827837dff448785d910e6071
shivsethuraman3267 <shivsethuraman@gmail.com>
2026-02-23 17:53:33 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "routine-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((cache) => cache.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=> cached))
+  );
+});
